f297e124169016a935b170e1a2a0e8b78796b11d
Tyler <tvalley93@gmail.com>
2021-05-05 23:24:37 -0500

    update server

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/'));
+app.use(express.static('./dist/portfolio'));
+
+app.get('/*', (req, res) => 
+    res.sendFile('index.html', {root: 'dist/portfolio/'}),
+);
 
 
 // Start the app by listening on the default Heroku port
